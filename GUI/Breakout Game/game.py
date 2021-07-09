fa216d90a288f99f54d08b375208112be7e981b8
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-09 13:52:18 +0300

    Defined constants and configuration for futture game such as score, lives, size of window and title.

--- GUI/Breakout Game/game.py
+++ GUI/Breakout Game/game.py
@@ -1,3 +1,19 @@
 import pygame
 
 pygame.init()
+
+# COLOR CONSTANTS
+WHITE = (255, 255, 255)
+DARK_BLUE = (36, 90, 190)
+LIGHT_BLUE = (0, 176, 240)
+RED = (255, 0, 0)
+ORANGE = (255, 100, 0)
+YELLOW = (255, 255, 0)
+
+# game parameters
+score = 0
+lives = 3
+size = (800, 600)
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
