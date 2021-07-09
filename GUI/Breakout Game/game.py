90447c4d59d92c069d72c0d7d4fb11684cf23a65
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-09 14:00:51 +0300

    Customize game window.
    Blue background with white texts - score and lives, located at the top of the screen above white line.

--- GUI/Breakout Game/game.py
+++ GUI/Breakout Game/game.py
@@ -17,3 +17,30 @@ size = (800, 600)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Breakout Game")
+
+game_is_on = True
+clock = pygame.time.Clock()
+
+while game_is_on:
+    for event in pygame.event.get():
+        # condition for quit
+        if event.type == pygame.QUIT:
+            game_is_on = False
+
+    # main logic of the game
+
+    # customize display
+    screen.fill(DARK_BLUE)
+    pygame.draw.line(screen, WHITE, [0, 38], [800, 38], 2)
+    font = pygame.font.Font(None, 34)
+    score_text = font.render(f"Your Score: {score}", 1, WHITE)
+    screen.blit(score_text, (20, 10))
+    lives_text = font.render(f"Lives left: {lives}", 1, WHITE)
+    screen.blit(lives_text, (650, 10))
+
+    # update display
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
