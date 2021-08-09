5afd77eac197dc641d217d8eb1d73bd1b60a0ded
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-09 09:22:06 +0300

    Improved Morse Code programm.
    In the morse_code.py file created 3 functions: 1) print menu; 2) works with user through the console in while loop;
    3) last creates the instanse of MorseCodeTranslator class, sets the program state to False and runs the first two functions.
    The main function runs in if statement.

--- Python Scripting/morse_code.py
+++ Python Scripting/morse_code.py
@@ -1,19 +1,42 @@
 from morse_code_translator import MorseCodeTranslator
 
-print("Greetings to Morse Code Translator")
-morse_code_translator = MorseCodeTranslator()
-app_is_on = False
 
-while not app_is_on:
-    user_operation = input("To encrypt message(type 'e') to decrypt (type 'd')? ").lower()
-    if user_operation == "e":
-        human_text = input("Please type your text: ")
-        morse_code_translator.text = human_text
-        print(f"Here is your encrypt text {morse_code_translator.encrypt_text()}")
-    elif user_operation == "d":
-        encrypt_text = input("Please type the encrypt text: ")
-        normal_text = morse_code_translator.decrypt_text(encrypt_text)
-        print(f"Here's the original text: {normal_text}")
-    user_operation = input("To continue press 'ENTER', to exit (type 'q') ").lower()
-    if user_operation == "q":
-        app_is_on = True
+def print_menu():
+    """Print the main menu into console"""
+    print(" \nGreetings to Morse Code Translator")
+    print("1) Make an encrypt code. \n2) Decode a message. \n3) Quit. \n")
+
+
+def app_logic(morse_code_program, condition: bool) -> None:
+    """Main logic of Morse Code encryption and decryption"""
+    while not condition:
+        user_operation = int(input("Enter your selection (from 1 to 3): "))
+        if user_operation == 1:
+            human_text = input("Please type your text: ")
+            morse_code_program.text = human_text
+            print(f"Here is your encrypt text {morse_code_program.encrypt_text()}")
+            print_menu()
+        elif user_operation == 2:
+            encrypt_text = input("Please type the encrypt text: ")
+            normal_text = morse_code_program.decrypt_text(encrypt_text)
+            print(f"Here's the original text: {normal_text}")
+            print_menu()
+        elif user_operation == 3:
+            condition = True
+            print("Thanks for using")
+        else:
+            print("Incorrect selection")
+            user_operation = input("To continue press 'ENTER' ")
+            print_menu()
+
+
+def main():
+    """Run all the main functionality"""
+    morse_code_translator = MorseCodeTranslator()
+    app_is_on = False
+    print_menu()
+    app_logic(morse_code_program=morse_code_translator, condition=app_is_on)
+
+
+if __name__ == "__main__":
+    main()
