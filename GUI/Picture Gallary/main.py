722405fea27115c467fcd0a6c7d7457ba29b45a0
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-09 15:00:18 +0300

    Created the tables for database.

--- GUI/Picture Gallary/main.py
+++ GUI/Picture Gallary/main.py
@@ -10,4 +10,11 @@ if __name__ == "__main__":
     window.mainloop()
     db = "art_gallery.db"
     with ArtGalleryDatabase(db) as conn_cursor:
-        print(conn_cursor.execute("select sqlite_version();").fetchall()[0][0])
+        # print(conn_cursor.execute("select sqlite_version();").fetchall()[0][0])
+        conn_cursor.execute("""CREATE TABLE IF NOT EXISTS Artists(
+        artistid integer PRIMARY KEY, name text, address text, town text, county text, postcode
+        text); """)
+        conn_cursor.execute("""CREATE TABLE IF NOT EXISTS Arts(
+        pieceid integer PRIMARY KEY, artistid integer not null, title text not null, medium text not null, 
+        price integer not null);
+        """)
