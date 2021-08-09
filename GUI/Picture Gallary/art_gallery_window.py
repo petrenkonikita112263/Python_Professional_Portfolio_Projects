a31ead65f06e62cc1f6a3c93c16ac7ac40f7f381
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-09 10:30:37 +0300

    Created new class file.
    In art_gallery_window.py was created the class that inherits from tk.Frame.
    In main.py creats the instance of that class.

---
+++ GUI/Picture Gallary/art_gallery_window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+class ArtGalleryWindow(tk.Frame):
+    """Tkinter window"""
+
+    def __init__(self, parent):
+        """From superclass created the actual frame"""
+        tk.Frame.__init__(self, parent)
--- GUI/Picture Gallary/main.py
+++ GUI/Picture Gallary/main.py
@@ -1,6 +1,9 @@
+from art_gallery_window import ArtGalleryWindow
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Art Gallery")
-window.geometry("1220x600")
-window.mainloop()
+if __name__ == "__main__":
+    window = tk.Tk()
+    main_program_window = ArtGalleryWindow(window)
+    window.title("Art Gallery")
+    window.geometry("1220x600")
+    window.mainloop()
