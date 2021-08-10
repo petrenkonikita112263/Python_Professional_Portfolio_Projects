dfcf96ca2e40a224561a06aa35d50161b5abbcdc
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-10 13:55:09 +0300

    improvements of main.py in Typing Speed Test application.

--- GUI/Typing Speed Test/main.py
+++ GUI/Typing Speed Test/main.py
@@ -1,7 +1,8 @@
 from app_window import Window
 import tkinter as tk
 
-root = tk.Tk()
-window = Window(root)
-root.title("Typing Speed Test")
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    window = Window(root)
+    root.title("Typing Speed Test")
+    root.mainloop()
