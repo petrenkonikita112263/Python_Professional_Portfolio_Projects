716a952daa9ed10eb35e3f09d67865350733c3e6
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-11 14:03:55 +0300

    Small improvements for app that adds watermark to the image.

--- GUI/Watermarker Editor Image/add_watermak.py
+++ GUI/Watermarker Editor Image/add_watermak.py
@@ -2,8 +2,9 @@ from tkinter_image_app import tk, TkinterImageApplication
 
 AUTHOR_MARK = "Dark Overmind"
 
-root = tk.Tk()
-root.wm_title("Watermarker Editor Image")
-root.geometry("100x100")
-app = TkinterImageApplication(AUTHOR_MARK, master=root)
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.wm_title("Watermarker Editor Image")
+    root.geometry("100x100")
+    app = TkinterImageApplication(AUTHOR_MARK, master=root)
+    app.mainloop()
