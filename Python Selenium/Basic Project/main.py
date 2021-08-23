b8e2062fce2e6f4ac852fcc8addeeb94ea513b52
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-23 12:34:40 +0300

    Updated main.py.
    Added constant with url link, which pass to webroser for testing.

--- Python Selenium/Basic Project/main.py
+++ Python Selenium/Basic Project/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
 DRIVER_PATH = r"C:\Users\Nikita\Documents\dev\msedgedriver.exe"
+TEST_URL = r"https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html"
 
 driver = webdriver.Edge(DRIVER_PATH)
+driver.get(TEST_URL)
