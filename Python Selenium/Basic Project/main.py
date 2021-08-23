abe64667c0beee19593d3813e8412712f5c17a0b
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-23 12:59:41 +0300

    Updated main.py.
    Removed two lines of codes with WebDriverWait by new imports for waiting custom condition.

--- Python Selenium/Basic Project/main.py
+++ Python Selenium/Basic Project/main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 DRIVER_PATH = r"C:\Users\Nikita\Documents\dev\msedgedriver.exe"
 TEST_URL = r"https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html"
@@ -9,5 +12,10 @@ driver.implicitly_wait(15)
 button = driver.find_element_by_id("downloadButton")
 button.click()
 
-progress_msg = driver.find_element_by_class_name("progress-label")
-print(f"{progress_msg.text}")
+# explicit wait - 30 seconds until the condition (text in progress bar would be Complete) is archived
+WebDriverWait(driver, 30).until(
+    ec.text_to_be_present_in_element(
+        (By.CLASS_NAME, "progress-label"),                  # element filtration
+        "Complete!"                                         # expected text
+    )
+)
