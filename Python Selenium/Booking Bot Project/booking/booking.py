55b835e299423ddf838e029a7d6a447182963032
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-31 09:52:29 +0300

    Updated booking.py.
    Added 20 sec implicit wait and open the browser window in max size.

--- Python Selenium/Booking Bot Project/booking/booking.py
+++ Python Selenium/Booking Bot Project/booking/booking.py
@@ -9,6 +9,8 @@ class Booking():
     def __init__(self, driver_path=const.DRIVER_PATH):
         """Constructor of the class"""
         self.browser = webdriver.Edge(driver_path)
+        self.browser.implicitly_wait(20)
+        self.browser.maximize_window()
 
     def get_home_page(self):
         """Load the url web page"""
