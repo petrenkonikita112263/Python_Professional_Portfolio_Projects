68ac56439e86727ece7ab337bb5b124a863595fc
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-31 10:29:50 +0300

    Updated main.py.
    Run new methods in main file.

--- Python Selenium/Booking Bot Project/main.py
+++ Python Selenium/Booking Bot Project/main.py
@@ -3,3 +3,6 @@ from booking.booking import Booking
 if __name__ == "__main__":
     booking_instance = Booking()
     booking_instance.get_home_page()
+    # this line can be run once, to change the currency value, by sending the currency type to it
+    # booking_instance.change_currency()
+    booking_instance.fill_place_name("Київ")
