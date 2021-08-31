1b75054d4082f1c5c45ddacab78bfd9af67a1a22
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-31 10:08:02 +0300

    Updated booking.py.
    Added new method change_currency, that finds first currency_element click on it
    and then try to change to new currency from the list and click on it, by default it's UAH.

--- Python Selenium/Booking Bot Project/booking/booking.py
+++ Python Selenium/Booking Bot Project/booking/booking.py
@@ -15,3 +15,15 @@ class Booking():
     def get_home_page(self):
         """Load the url web page"""
         self.browser.get(const.HTML_URL)
+
+    def change_currency(self, currency="UAH"):
+        """Change the type of currency on the page.
+        By default in my country it's UAH."""
+        currency_element = self.browser.find_element_by_css_selector(
+            "button[data-tooltip-text='Виберіть валюту']"
+        )
+        currency_element.click()
+        selected_currency_element = self.browser.find_element_by_css_selector(
+            f"a[data-modal-header-async-url-param*='selected_currency={currency}']"
+        )
+        selected_currency_element.click()
