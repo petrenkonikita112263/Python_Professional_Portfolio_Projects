4319a5c5d24bd0e3fafee286f34cb43347ef02b6
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-01 20:17:05 +0300

    Updated main.py.
    Run new functionality for booking bot.

--- Python Selenium/Booking Bot Project/main.py
+++ Python Selenium/Booking Bot Project/main.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     # this line can be run once, to change the currency value, by sending the currency type to it
     # booking_instance.change_currency()
     booking_instance.fill_place_name("Київ")
+    booking_instance.select_dates("2021-09-04", "2021-09-09")
+    booking_instance.select_adult_human(5)
+    booking_instance.search_info()
