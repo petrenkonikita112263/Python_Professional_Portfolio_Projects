37d6691829e9c2d76feb552455052f69259e434f
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-02 20:14:51 +0300

    Updated booking_filtration.py.
    Added new method to filter the room by stars.

--- Python Selenium/Booking Bot Project/booking/booking_filtration.py
+++ Python Selenium/Booking Bot Project/booking/booking_filtration.py
@@ -6,3 +6,13 @@ class BookingFiltration:
 
     def __init__(self, driver: WebDriver) -> None:
         self.driver = driver
+
+    def apply_star_rating(self, star_value=3):
+        """Filtration by ratings in start, by default chooses the 3 stars."""
+        star_box = self.driver.find_element_by_id("filter_class")
+        star_child_element = star_box.find_elements_by_css_selector(
+            "*"
+        )
+        for star_element in star_child_element:
+            if str(star_element.get_attribute("innerHTML")).strip() == f"{star_value} зірки":
+                star_element.click()
