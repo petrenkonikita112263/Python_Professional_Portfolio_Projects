86becfe37248b9fad028f5d1c437223989d54dd2
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-02 16:58:03 +0300

    Updated apps.py.
    In projectp app renamed class to the app name.

--- Simple Django Project/juggler/projectp/apps.py
+++ Simple Django Project/juggler/projectp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjecttpConfig(AppConfig):
+class ProjectpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'projecttp'
+    name = 'projectp'
