74405b5a80526f9051595206d87e4fcc8f02f871
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-04 18:39:43 +0300

    Updated booking_filtration.py.
    Added two more methods for filttation by highest user's ratings and ability for walking
    from appartment(hotel).

--- Python Selenium/Booking Bot Project/booking/booking_filtration.py
+++ Python Selenium/Booking Bot Project/booking/booking_filtration.py
@@ -16,3 +16,21 @@ class BookingFiltration:
         for star_element in star_child_element:
             if str(star_element.get_attribute("innerHTML")).strip() == f"{star_value} зірки":
                 star_element.click()
+
+
+    def sort_by_most_user_ratings(self):
+        """Use another filter to filter by stars (rating)."""
+        high_ratings = self.driver.find_element_by_css_selector(
+            "li[data-id='class']"
+        )
+        high_ratings.click()
+
+    def apply_walking(self):
+        """Filtration by foot walking at downtime."""
+        activities_option = self.driver.find_element_by_xpath(
+            "/html/body/div[3]/div/div[4]/div[1]/div[2]/div[1]/div[6]/form/fieldset/div[1]/div/div[13]/div[2]"
+        )
+        foot_walking = activities_option.find_element_by_css_selector(
+            "a[data-id='popular_activities-70']"
+        )
+        foot_walking.click()
