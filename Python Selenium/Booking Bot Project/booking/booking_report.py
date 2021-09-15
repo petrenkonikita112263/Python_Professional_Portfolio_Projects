ce22a3757a6caae5c58da0cbdfe36610feafe420
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-15 12:16:42 +0300

    Created Python Selenium/Booking Bot Project/booking/booking_report.py.
    New Python class with constructor.

---
+++ Python Selenium/Booking Bot Project/booking/booking_report.py
@@ -0,0 +1,4 @@
+class BookingReport:
+    """Class with method that will parse the hotel list from html page."""
+    def __init__(self, boxes_section_element):
+        self.boxes_section_element = boxes_section_element
