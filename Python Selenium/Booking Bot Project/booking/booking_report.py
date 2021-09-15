0f7689009a6d42e58f138bbc9140a59b56fbf838
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-15 12:24:19 +0300

    Updated Python Selenium/Booking Bot Project/booking/booking_report.py.
    Created method pull_deal_boxes that is called from init method of the class.

--- Python Selenium/Booking Bot Project/booking/booking_report.py
+++ Python Selenium/Booking Bot Project/booking/booking_report.py
@@ -1,4 +1,12 @@
 class BookingReport:
     """Class with method that will parse the hotel list from html page."""
+
     def __init__(self, boxes_section_element):
         self.boxes_section_element = boxes_section_element
+        self.deal_boxes = self.pull_deal_boxes()
+
+    def pull_deal_boxes(self):
+        """Return the list of hotels from main box in result (1st) page."""
+        return self.boxes_section_element.find_elements_by_class_name(
+            "sr_property_block"
+        )
